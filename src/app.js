d7de9b1abe12a7a393194a166342289dc1373ac6
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-03-09 18:18:41 +0530

    removed trailing slash from the cors

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 app.use(
     cors({
-        origin: ["https://movie-hub-beta-ruddy.vercel.app/", "http://localhost:5173"],
+        origin: ["https://movie-hub-beta-ruddy.vercel.app", "http://localhost:5173"],
         credentials: true,
     })
 );
